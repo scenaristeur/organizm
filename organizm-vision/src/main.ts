c9d640ff0d81964a4ea8600f690f507246524d00
scenaristeur <scenaristeur@gmail.com>
2024-11-17 12:18:58 +0100

    photogalery

--- organizm-vision/src/main.ts
+++ organizm-vision/src/main.ts
@@ -1,3 +1,6 @@
+// Above the createApp() line
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
